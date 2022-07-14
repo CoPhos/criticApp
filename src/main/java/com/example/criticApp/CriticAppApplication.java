8ad3b790115895ba19f3b32120a6936552036c7b
Jorge Cobos <cobos1932@gmail.com>
2022-07-14 17:57:34 -0500

    First commit from command line

---
+++ src/main/java/com/example/criticApp/CriticAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.criticApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriticAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriticAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/criticApp/CriticAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.criticApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriticAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
